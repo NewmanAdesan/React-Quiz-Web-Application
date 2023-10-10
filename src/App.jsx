6a5e6df86a00c9358b2ec6125554cbb684939166
NewmanAdesan <newmanadesan@gmail.com>
2023-10-10 15:40:20 +0100

    feat: Add quiz questions data from fake API
    - Created 'questions.json' with 15 questions, options, correct answers, and points allocation.
    - Used json-server to serve the data at http://localhost:9000/
    - Implemented data fetching in 'App' component using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+import { useReducer } from 'react';
+
 import './App.css'
 import Header from './components/Header';
 
+
+const initialState = {
+  questions: [],
+  // "loading", "error", "ready", "active", finished
+  status: "loading",
+}
+
+const reducer = function (state, action) {
+
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      }
+
+    case "dataFailed":
+      console.log("There was an Error Fetching the Data.")
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error("Incorrect Action Type");
+  }
+
+}
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(()=>{
+    fetch("http://localhost:9000/questions")
+      .then(res => res.json())
+      .then(data => dispatch({type:"dataReceived", payload:data}))
+      .catch(data => dispatch({type:"dataFailed", payload:null}))
+  }, [])
 
   return (
     <div className='app'>
